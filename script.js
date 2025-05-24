5f3117c11c96c1ced465aa7067f5a251e66bba7e
A Q <aq@As-MacBook-Air.local>
2025-05-24 03:41:04 +0300

    شرح التعديل

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ fetch("https://opensheet.elk.sh/1D4gfAqMfJbhYE6xkpb7ki42fiPg4Leob5o9NbmCDaPE/She
     document.getElementById("description").textContent = product.description;
     document.getElementById("imgg").src = product.image;
     document.getElementById("ctaBtn").textContent = product.buttonText;
+    document.getElementById("title2").textContent = product.title2;
+    document.getElementById("description2").textContent = product.description2;
+    document.getElementById("imgg2").src = product.image2;
+    document.getElementById("ctaBtn2").textContent = product.buttonText2;
   })
   .catch(error => {
     console.error("فشل في تحميل البيانات:", error);
